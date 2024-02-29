b4f469632886a6bbf3e0b4d56e1771149f0f1061
Emma <120310616@umail.ucc.ie>
2024-02-29 11:41:42 +0000

    Finish RA and start graph Partition decider

--- other/testRADecider.py
+++ other/testRADecider.py
@@ -3,7 +3,7 @@ from src.setupGrid import warehouseFloor, graphManger
 from src.agent import agent
 from src.reservedArea.reservedAgentsDecider import reservedAgentDecider
 
-
+"""
 graph = warehouseFloor(7, 8)
 graph.setStaticObstacle([[0,3],[1,3],[2,3],[3,3]])
 graphm = graphManger(graph)
@@ -11,5 +11,18 @@ agent1 = agent(1, [2,2], [4,7])
 agent2 = agent(2, [0,5], [5,5])
 agents = [agent1,agent2]
 a = reservedAgentDecider(graph, agents)
-a.attemptTwo()
+a.attemptTwo(1)
+"""
+
+
+graph = warehouseFloor(11, 11)
+graphm = graphManger(graph)
+agent1 = agent(1, [1,3], [1,9])
+agent2 = agent(2, [7,3], [7,9])
+agent3 = agent(3, [5,6], [10,6])
+agents = [agent2,agent3]
+a = reservedAgentDecider(graph, agents)
+a.attemptTwo(2)
+
+
 
--- src/graphPartition/graphPartitionDecider.py
+++ src/graphPartition/graphPartitionDecider.py
@@ -1,1 +1,42 @@
+from src.aStar import aStar
+from src.setupGrid import graphManger
+from collections import defaultdict
+class graphPartitionDecider:
+    def __init__(self,graph, agents ):
+        self.initialGraph = graph
+        self.agents = agents
+
+    #Linearly split up graphs
+    def randomisePartition(self):
+        pass
+
+
+
+    # Popularity Percentage - what percentage is considered popular
+    # BufferRatio - what percentage of the original graph should be considered for a buffer area
+    #should also put a restriction on how small a partition can exist within the graph
+    def graphAnalysis(self, popularityPercentage, bufferRatio):
+        graphM = graphManger(self.graph)
+        aStarObj = aStar(graphM)
+        nodes = defaultdict(int)
+        count = 0
+        for agent in self.agents:
+            path = aStarObj.findPath([], agent,self.graph.width * self.graph.length)
+            if path is False:
+                return False
+            for step in path:
+                nodes[step.x, step.y] += 1
+            count += len(path)
+        popularityThres = count * popularityPercentage
+        freqVisited = [key for key, count in nodes.items() if count > popularityThres]
+        for popVal in freqVisited:
+            pass
+            #build buffer area around each popVal
+            #if it intersects, merge partitions
+
+
+
+        #should disregard nodes with count 1 - visited by one node only
+
+
 
